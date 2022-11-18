80ab9eb18868918492aacfb6dde909d4b1fb8606
duyduyttt113 <117121766+duyduyttt113@users.noreply.github.com>
2022-11-19 03:39:24 +0700

    update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose')
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb+srv://duyduyttt113:bchqshtlco@cluster0.ozhm5ll.mongodb.net/data', {
-                                // mongodb+srv://duyduyttt113:bchqshtlco@cluster0.ozhm5ll.mongodb.net/data?retryWrites=true&w=majority
-                                // mongodb+srv://vietdung:jy9ACEBmidW2hPVZ@cluster0.ly444.mongodb.net/data?retryWrites=true&w=majority
+        await mongoose.connect('mongodb+srv://duyduyttt113:bchqshtlco@cluster0.ozhm5ll.mongodb.net/data?retryWrites=true&w=majority', {
+
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
